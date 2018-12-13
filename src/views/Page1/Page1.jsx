9f00e65271b600a39073ad1ccf6b888defbb13e6
vivamau <maurizio.blasilli@gmail.com>
2018-12-13 23:25:32 +0100

    added the state update method to pass values from query-string

--- src/views/Page1/Page1.jsx
+++ src/views/Page1/Page1.jsx
@@ -2,19 +2,28 @@ import React, { Component } from "react";
 import QueryString from "query-string";
 class Page1 extends Component {
 
+    getParameters = () => {
+        const values = QueryString.parse(this.props.location.search);
+        return (values)
+    }
 
+    //constructor(props) {
+    //    super(props)
+    //    this.state = { }
+    //}
 
-getParameters = () => {
-    const values = QueryString.parse(this.props.location.search);
-    return (values)
-}
-render() {
-    return (
-        <div>
-            <h1>Page1</h1>
-            ID Parameters passed is test: {this.getParameters().id};
-        </div>
-    )
-}
+    //componentDidMount() {
+    //    this.setState(QueryString.parse(this.props.location.search));
+    //}
+   // ID Parameters passed is test: {this.state.id}
+   
+    render() {
+        return (
+            <div>
+                <h1>Page1</h1>
+                ID Parameters passed is test: {this.getParameters().id}
+            </div>
+        )
+    }
 }
 export default Page1;
